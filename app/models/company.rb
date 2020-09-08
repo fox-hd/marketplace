c84e892f22ac05c03dd48f36a351d2b0e907705a
ueslei neves <unp1210@gmail.com>
2020-09-08 14:18:27 -0300

    Adicionando metodo em model Company

--- app/models/company.rb
+++ app/models/company.rb
@@ -2,4 +2,12 @@ class Company < ApplicationRecord
   has_many :users, dependent: :destroy
   has_many :profiles, dependent: :destroy
   has_many :products, dependent: :destroy
+
+  before_create :name_domain
+
+  private
+
+  def name_domain
+    self.name = email.split('@')[1].split('.')[0].capitalize
+  end
 end
